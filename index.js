3f10cae74a6f7866211d0f458226a434787ef878
brandong1 <brandon@brandonleeproductions.com>
2020-03-16 16:14:49 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution here!
+const append = ["Milo", "Otis", "Garfield"], 
+      prepend = ["Milo", "Otis", "Garfield"], 
+      removeLast = ["Milo", "Otis", "Garfield"], 
+      removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push('Odie') //Adds 'Odie' to end of array.
+
+prepend.unshift('Odie') //Adds 'Odie' to beginning of array
+
+removeLast.pop('Garfield') //Removes 'Garfield' from end of array
+
+removeFirst.shift('Milo') //Removes 'Milo' from beginning of array
